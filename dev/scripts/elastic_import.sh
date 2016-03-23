60e1ec0cf5479bcf9ddefd63014b89d4272d0a9e
Anthony <aquinault@gmail.com>
2016-03-23 11:51:21 +0100

    add script import export elastic search

--- dev/scripts/elastic_import.sh
+++ dev/scripts/elastic_import.sh
@@ -6,28 +6,28 @@ URL1="http://$USER:$PASS@es.cadesoft.com/annonces"
 URL2="http://$USER:$PASS@es.cadesoft.com/adimages"
 
 # Restore index data to a file (ie : stored in /tmp/myESdumps) :
-echo "Export annonces Data"
+echo "Import annonces Data"
 docker run --rm -ti -v /tmp/myESdumps:/data elasticdump \
   --output=$URL1 \
   --input=/data/my_annonces.json \
   --type=data
 
-echo "Export annonces Mapping"
+echo "Import annonces Mapping"
 docker run --rm -ti -v /tmp/myESdumps:/data elasticdump \
   --output=$URL1 \
   --input=/data/my_annonces_mapping.json \
   --type=mapping
 
-echo "Export adimages Data"
+echo "Import adimages Data"
 docker run --rm -ti -v /tmp/myESdumps:/data elasticdump \
   --output=$URL2 \
   --input=/data/my_adimages.json \
   --type=data
 
-echo "Export adimages Mapping"
+echo "Import adimages Mapping"
 docker run --rm -ti -v /tmp/myESdumps:/data elasticdump \
   --output=$URL2 \
   --input=/data/my_adimages_mapping.json \
   --type=mapping
 
-echo "Export Done!"
+echo "Import Done!"
